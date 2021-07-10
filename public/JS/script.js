2f2939763bcce4e1ad392b9f078be282df500961
Revika Anand <61104353+Revika-Anand@users.noreply.github.com>
2021-07-09 21:50:15 -0500

    Update script.js
    changed port

--- public/JS/script.js
+++ public/JS/script.js
@@ -10,7 +10,7 @@ const user = prompt("Enter your name");
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "3000"
+  port: "443"
 });
 
 //initialization and setting of my video
